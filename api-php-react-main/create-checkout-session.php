79d7bf5a3043a4d975d3f65278133054c641fdad
kekoinomano <sergioalvarezalcedo@gmail.com>
2022-02-13 16:40:01 +0100

    librerias

---
+++ api-php-react-main/create-checkout-session.php
@@ -0,0 +1,30 @@
+<?php
+include "./libs/stripe-php-master/init.php";
+//require 'vendor/autoload.php';
+// This is a public sample test API key.
+// Don’t submit any personally identifiable information in requests made with this key.
+// Sign in to see your own test API key embedded in code samples.
+\Stripe\Stripe::setApiKey('sk_test_H08xc050YQnMm56SsKcSIGPU');
+
+header('Content-Type: application/json');
+
+$YOUR_DOMAIN = 'http://localhost:3000';
+
+$checkout_session = \Stripe\Checkout\Session::create([
+  'line_items' => [[
+    'price_data' => [
+      'currency' => 'usd',
+      'product_data' => [
+        'name' => 'T-shirt',
+      ],
+      'unit_amount' => 2000,
+    ],
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+    'success_url' => $YOUR_DOMAIN . '?success.html',
+    'cancel_url' => $YOUR_DOMAIN . '?canceled=true',
+]);
+
+header("HTTP/1.1 303 See Other");
+header("Location: " . $checkout_session->url);
--- api-php-react-main/funciones.php
+++ api-php-react-main/funciones.php
@@ -84,10 +84,58 @@ function obtenerConexion()
     $password = obtenerVariableDelEntorno("MYSQL_PASSWORD");
     $user = obtenerVariableDelEntorno("MYSQL_USER");
     $dbName = obtenerVariableDelEntorno("MYSQL_DATABASE_NAME");
-    $database = new PDO('mysql:host=localhost;dbname=' . $dbName, $user, $password);
+    $dbHost = obtenerVariableDelEntorno("MYSQL_HOST");
+    $database = new PDO('mysql:host='. $dbHost .';dbname=' . $dbName, $user, $password);
     $database->query("set names utf8;");
     $database->setAttribute(PDO::ATTR_EMULATE_PREPARES, FALSE);
     $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $database->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     return $database;
+}
+
+function _email($email, $subject, $body, $is_html = false, $only_smtp = false) {
+    /* set header */
+    $destino="sergioalvarezalcedo@gmail.com";
+    $header  = "MIME-Version: 1.0\r\n";
+    $header .= 'From: Your name <info@address.com>' . "\r\n";
+    //$headers = "From: " . strip_tags($destino) . "\r\n";
+    //$headers .= "Reply-To: ". strip_tags($destino) . "\r\n";
+    if($is_html) {
+        $header .= "Content-Type: text/html; charset=\"utf-8\"\r\n";
+    } else {
+        $header .= "Content-Type: text/plain; charset=\"utf-8\"\r\n";
+    }
+    /* send email */
+        /* SMTP */
+        require_once('libs/PHPMailer/PHPMailer.php');
+        require_once('libs/PHPMailer/SMTP.php');
+        require_once('libs/PHPMailer/Exception.php');
+        $mail = new PHPMailer\PHPMailer\PHPMailer;
+        $mail->CharSet = "UTF-8";
+        $mail->isSMTP();
+        $mail->Host = "ssl0.ovh.net";
+        $mail->SMTPAuth = true;
+        $mail->Username = "equipo@wakeapp.org";
+        $mail->Password = "wqA_vynf4R4#)p%";
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = "465";
+        $setfrom = $mail->Username;
+        $mail->setFrom("equipo@wakeapp.org", "Equipo");
+        $mail->addAddress($email);
+        $mail->Subject = $subject;
+        if($is_html) {
+            $mail->isHTML(true);
+            $mail->AltBody = strip_tags($body);
+        }
+        $mail->Body = $body;
+        if(!$mail->send()) {
+            if($only_smtp) {
+                return false;
+            }
+            /* send using mail() */
+            if(!mail($email, $subject, $body, $header)) {
+                return false;
+            }
+        }
+    return true;
 }
